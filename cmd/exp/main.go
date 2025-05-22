0e385586150313ff381930b08ba1d9609b951037
anoriqq <shota.yoshikawa@anoriqq.com>
2025-05-22 12:42:01 +0900

    feat: handle error

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	fmt.Println("Hello World")
+	return nil
 }
