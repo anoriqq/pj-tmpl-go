380b0faefe25cfa8783bf9cd105fddf28a2ec4be
anoriqq <shota.yoshikawa@anoriqq.com>
2025-06-25 01:32:15 +0900

    test: add test for options

--- cmd/server/internal/cli/options_test.go
+++ cmd/server/internal/cli/options_test.go
@@ -1,1 +1,34 @@
 package cli_test
+
+import (
+	"testing"
+
+	"github.com/anoriqq/pj-tmpl-go/cmd/server/internal/cli"
+	"github.com/brianvoe/gofakeit/v7"
+)
+
+func TestNewOptions(t *testing.T) {
+	t.Run("環境変数からオプションを取得できる", func(t *testing.T) {
+		want := gofakeit.Word()
+		t.Setenv("ENV", want)
+
+		// Act
+		got := cli.NewOptions()
+
+		// Assert
+		if got.Env() != want {
+			t.Errorf("envは %s を得るはずが %s を得た", want, got.Env())
+		}
+	})
+
+	t.Run("デフォルト値を取得できる", func(t *testing.T) {
+		// Act
+		got := cli.NewOptions()
+
+		// Assert
+		want := ""
+		if got.Env() != want {
+			t.Errorf("envは %s を得るはずが %s を得た", want, got.Env())
+		}
+	})
+}
