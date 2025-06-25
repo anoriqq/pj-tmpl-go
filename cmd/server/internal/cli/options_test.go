4a69f6b8cff821e77b3f60068843fc1ea28e7e7d
anoriqq <shota.yoshikawa@anoriqq.com>
2025-06-25 23:37:54 +0900

    test: 並列化を無効化して書き直し

--- cmd/server/internal/cli/options_test.go
+++ cmd/server/internal/cli/options_test.go
@@ -1,6 +1,8 @@
 package cli_test
 
 import (
+	"flag"
+	"fmt"
 	"testing"
 
 	"github.com/anoriqq/pj-tmpl-go/cmd/server/internal/cli"
@@ -8,18 +10,7 @@ import (
 )
 
 func TestNewOptions(t *testing.T) {
-	t.Run("環境変数からオプションを取得できる", func(t *testing.T) {
-		want := gofakeit.Word()
-		t.Setenv("ENV", want)
-
-		// Act
-		got := cli.NewOptions()
-
-		// Assert
-		if got.Env() != want {
-			t.Errorf("envは %s を得るはずが %s を得た", want, got.Env())
-		}
-	})
+	// [flag] packageの操作がスレッドセーフでないので並列化しない
 
 	t.Run("デフォルト値を取得できる", func(t *testing.T) {
 		// Act
@@ -28,7 +19,57 @@ func TestNewOptions(t *testing.T) {
 		// Assert
 		want := ""
 		if got.Env() != want {
-			t.Errorf("envは %s を得るはずが %s を得た", want, got.Env())
+			t.Errorf("デフォルト値である`%s`を得るはずが`%s`を得た", want, got.Env())
+		}
+	})
+
+	t.Run("環境変数からオプションを取得できる", func(t *testing.T) {
+		want := gofakeit.Word()
+		t.Setenv("ENV", want)
+		t.Logf("環境変数を設定: %s", want)
+
+		// Act
+		got := cli.NewOptions()
+
+		// Assert
+		if got.Env() != want {
+			t.Errorf("設定した`%s`を得るはずが`%s`を得た", want, got.Env())
+		}
+	})
+
+	t.Run("フラグからオプションを取得できる", func(t *testing.T) {
+		want := gofakeit.Word()
+		if err := flag.CommandLine.Set("env", want); err != nil {
+			t.Fatalf("flagの設定に失敗: %v", err)
+		}
+		t.Logf("flagを設定: %s", want)
+
+		// Act
+		got := cli.NewOptions()
+
+		// Assert
+		if got.Env() != want {
+			t.Errorf("設定した`%s`を得るはずが`%s`を得た", want, got.Env())
+		}
+	})
+
+	t.Run("環境変数とフラグの両方が設定されている場合はフラグが優先される", func(t *testing.T) {
+		fromEnv := gofakeit.Word()
+		t.Setenv("ENV", fmt.Sprint(fromEnv))
+		t.Logf("環境変数を設定: %s", fromEnv)
+
+		fromFlag := gofakeit.Word()
+		if err := flag.CommandLine.Set("env", fmt.Sprint(fromFlag)); err != nil {
+			t.Fatalf("flagの設定に失敗: %v", err)
+		}
+		t.Logf("flagを設定: %s", fromFlag)
+
+		// Act
+		got := cli.NewOptions()
+
+		// Assert
+		if got.Env() != fromFlag {
+			t.Errorf("flagで設定した`%s`を得るはずが`%s`を得た", fromFlag, got.Env())
 		}
 	})
 }
