d2fa4522a79e071e10cb50e753908224d78f289c
anoriqq <shota.yoshikawa@anoriqq.com>
2025-08-11 14:51:06 +0900

    chore: enable gosec linter

--- internal/domain/port/port.go
+++ internal/domain/port/port.go
@@ -3,12 +3,15 @@ package port
 import (
 	"flag"
 	"fmt"
+	"math"
 	"strconv"
 
 	"github.com/go-errors/errors"
 )
 
-const maxPortValue = 65535
+const maxPortValue = math.MaxUint16
+
+var ErrInvalidPort = fmt.Errorf("port number must be between 0 and %d", maxPortValue)
 
 // Port ポート番号。
 type Port struct {
@@ -22,6 +25,9 @@ func (p *Port) Set(s string) error {
 		return errors.Wrap(err, 0)
 	}
 
+	if v > maxPortValue {
+		return errors.Wrap(ErrInvalidPort, 0)
+	}
 	*p = New(uint16(v))
 
 	return nil
